3955e2b871535fd58989331d41bb563c5c2e78c4
Uzeyr OZCAN <muzeyr@gmail.com>
2021-01-17 20:59:29 +0300

    arka plan resmi değiştirme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { CdkDragDrop } from '@angular/cdk/drag-drop';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'picture-book';
+  @ViewChild('mainDiv') main; 
+  public style: string;
+
+
+  constructor(){
+    
+  }
+  ngOnInit(){
+    //this.changeBack();
+
+  }
+  changeBack(): any {
+    console.log(this.main);
+    this.style  = "background-image: url(../assets/images/booktint.png);";
+    this.style +="background-repeat: repeat-y";
+    this.main.nativeElement.style = this.style
+  }
+  drop(event: CdkDragDrop<string[]>) {
+    console.log(event);
+  }
 }
